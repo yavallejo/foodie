1d4345ca220631a08239a39267911a018c0e138b
Yan Vallejo <yavallejor@gmail.com>
2025-07-18 16:35:13 -0500

    feat: implement MainHeader with Logo and Navigation, add ImageSlider component, and create styles for layout and header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import MainHeader from "@/components/MainHeader";
+import MainHeader from "@/components/header/MainHeader";
 import "./globals.css";
 
 export const metadata = {
--- components/header/MainHeader.tsx
+++ components/header/MainHeader.tsx
@@ -1,6 +1,6 @@
 import classes from "./MainHeader.module.css";
 
-import MainHeaderBackground from "@/components/MainHeaderBackground";
+import MainHeaderBackground from "@/components/header/MainHeaderBackground";
 import NavigationHeader from "./NavigationHeader";
 import Logo from "./Logo";
 const MainHeader = () => {
